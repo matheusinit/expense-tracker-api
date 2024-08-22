dfae065ff09c32660f8c0bf7b709f873faad7a6e
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-22 09:08:18 -0300

    chore: set up a set of essentials rules for TypeScript code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,22 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-
+import globals from 'globals'
+import parserTs from '@typescript-eslint/parser'
+import stylisticTs from '@stylistic/eslint-plugin-ts'
+import pluginJs from '@eslint/js'
+import tseslint from 'typescript-eslint'
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.node }},
+  { 'files': ['**/*.{ts}'] },
+  { 'languageOptions': { 'globals': globals.node, 'parser': parserTs } },
+  {
+    'plugins': {
+      '@stylistic/ts': stylisticTs
+    },
+    'rules': {
+      '@stylistic/ts/indent': ['error', 2],
+      '@stylistic/ts/quotes': ['error', 'single'],
+      '@stylistic/ts/semi': ['error', 'never'],
+    }
+  },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-];
+  ...tseslint.configs.recommended
+]
