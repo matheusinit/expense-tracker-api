a345dd5e56fb2e9cdfa0b8b83d3057a40ed9dbb9
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-23 07:46:50 -0300

    refactor: move description validation to a setter method

--- src/entities/expense.ts
+++ src/entities/expense.ts
@@ -3,16 +3,20 @@ export class Expense {
   private _amount!: number
 
   constructor(description: string, amount: number) {
-    if (!description) {
-      throw new Error('Description is required')
-    }
-
-    this._description = description
+    this.description = description
     this.amount = amount
   }
 
   get description(): string {
     return this._description
+  }
+
+  set description(value: string) {
+    if (!value) {
+      throw new Error('Description is required')
+    }
+
+    this._description = value
   }
 
   get amount(): number {
