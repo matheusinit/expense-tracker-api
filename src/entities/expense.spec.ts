cda40e80c2e81b620e128903d6aa69b93f6bf7eb
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-23 07:38:22 -0300

    test: expense should throw an exception if amount equals 0 is provided

--- src/entities/expense.spec.ts
+++ src/entities/expense.spec.ts
@@ -7,4 +7,10 @@ describe('Given is needed to create a expense,', () => {
 
     expect(classInstantiation).toThrowError('Description is required')
   })
+
+  it('when amount equals 0 is provided, should throw an exception', () => {
+    const classInstantiation = () => new Expense('Credit card bill', 0)
+
+    expect(classInstantiation).toThrowError('Amount is required. It should be greater than 0')
+  })
 })
