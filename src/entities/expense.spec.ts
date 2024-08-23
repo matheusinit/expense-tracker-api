20743a8df9c1975392be483656f9a6c4def25153
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-23 08:09:19 -0300

    test: expense should throw an exception if description length is greater than 255 is provided

--- src/entities/expense.spec.ts
+++ src/entities/expense.spec.ts
@@ -8,6 +8,18 @@ describe('Given is needed to create a expense,', () => {
     expect(classInstantiation).toThrowError('Description is required')
   })
 
+  it('when a description greater than 255 of length is provided, should throw an exception', () => {
+
+    const description = 'Eos laborum labore consequuntur voluptatem beatae in eos, \
+    repellat possimus voluptate quos cupiditate dicta vel. Ipsum esse occaecati \
+    magnam. Dolores labore qui deserunt unde qui, sequi at sequi doloribus beatae \
+    facilis tenetur. Est repellat deserunt ducb.'
+
+    const classInstantiation = () => new Expense(description, 100)
+
+    expect(classInstantiation).toThrowError('Description length cannot be greather than 255')
+  })
+
   it('when amount equals 0 is provided, should throw an exception', () => {
     const classInstantiation = () => new Expense('Credit card bill', 0)
 
