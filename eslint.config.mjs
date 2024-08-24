faafcba1c7d5c0746ee741e4c320bbd37484a8e2
suetham <matheus.oliveira.s@protonmail.com>
2024-08-24 20:40:16 -0300

    chore: add basic ESLint rules for TypeScript code

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,22 +1,31 @@
 import globals from 'globals'
 import parserTs from '@typescript-eslint/parser'
+import stylistic from '@stylistic/eslint-plugin'
 import stylisticTs from '@stylistic/eslint-plugin-ts'
 import pluginJs from '@eslint/js'
 import tseslint from 'typescript-eslint'
 
 export default [
-  { 'files': ['**/*.{ts}'] },
-  { 'languageOptions': { 'globals': globals.node, 'parser': parserTs } },
+  { files: ['**/*.{ts, mjs}'] },
+  { languageOptions: { globals: globals.node, parser: parserTs } },
   {
-    'plugins': {
-      '@stylistic/ts': stylisticTs
+    plugins: {
+      '@stylistic/ts': stylisticTs,
+      '@stylistic': stylistic
     },
-    'rules': {
+    rules: {
       '@stylistic/ts/indent': ['error', 2],
       '@stylistic/ts/quotes': ['error', 'single'],
       '@stylistic/ts/semi': ['error', 'never'],
-    }
+      '@stylistic/no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+      '@stylistic/no-multi-spaces': 'error',
+      '@stylistic/no-trailing-spaces': 'error',
+      '@stylistic/object-curly-spacing': ['error', 'always'],
+      '@stylistic/rest-spread-spacing': ['error', 'never'],
+      '@stylistic/function-paren-newline': ['error', 'consistent'],
+      '@stylistic/no-multiple-empty-lines': ['error', { 'max': 1, 'maxEOF': 0, 'maxBOF': 0 }]
+    },
   },
   pluginJs.configs.recommended,
-  ...tseslint.configs.recommended
+  ...tseslint.configs.recommended,
 ]
