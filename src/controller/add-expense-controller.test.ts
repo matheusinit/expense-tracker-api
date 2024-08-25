2bc8378ef955ecd2946a9b917f0b171d475759a6
suetham <matheus.oliveira.s@protonmail.com>
2024-08-25 14:56:24 -0300

    test: ensure when required fields is missing, then should return bad request and message error

--- src/controller/add-expense-controller.test.ts
+++ src/controller/add-expense-controller.test.ts
@@ -11,6 +11,10 @@ type ExpenseResponseDTO = {
   deletedAt?: string
 }
 
+type MessageErrorDTO = {
+  message: string
+}
+
 describe('Given add expense controller', () => {
   it('when required data is provided, then should return the data in response body', async () => {
     const expense = {
@@ -24,5 +28,18 @@ describe('Given add expense controller', () => {
 
     expect(responseBody.description).toEqual(expense.description)
     expect(responseBody.amount).toEqual(expense.amount)
+  })
+
+  it('when required field description is missing, then should return bad request status and message error', async () => {
+    const expense = {
+      amount: 100
+    }
+
+    const response = await request(app).post('/v1/expenses').send(expense)
+
+    const responseBody: MessageErrorDTO = response.body
+
+    expect(response.status).toEqual(400)
+    expect(responseBody.message).toEqual('Missing required fields: description')
   })
 })
