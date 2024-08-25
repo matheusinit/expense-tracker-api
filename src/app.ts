c40c0e3f52f6d5a978d92c24db17b16fdf0ca779
suetham <matheus.oliveira.s@protonmail.com>
2024-08-25 11:06:59 -0300

    fix: use helmet to apply security for HTTP response headers

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import AddExpenseController from './controller/add-expense-controller'
 import 'dotenv/config'
+import helmet from 'helmet'
 import ExpenseRepository from './repository/expense-repository'
 
 const app = express()
 
+app.use(helmet())
 app.use(express.json())
 
 const expenseRepository = new ExpenseRepository()
