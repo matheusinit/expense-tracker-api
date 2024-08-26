fab9fdd3bfa8cf8af08e31ab81da6a7183bf123e
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-26 20:22:32 -0300

    refactor: rewrite test description using BDD

--- src/middleware/csrf.spec.ts
+++ src/middleware/csrf.spec.ts
@@ -3,7 +3,7 @@ import request from 'supertest'
 import app from '../app'
 
 describe('CSRF Middleware', () => {
-  it('should return 403 status when CSRF token is missing', async () => {
+  it('when CSRF token is missing, then should return 403 status', async () => {
     const response = await request(app).post('/v1/expenses').send({})
 
     expect(response.status).toEqual(403)
