f66c41095e014f0dc5b86570164306d9829a952d
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-26 22:38:46 -0300

    feat: given CSRF middleware, when csrf token is missing, then should return a message error

--- src/app.ts
+++ src/app.ts
@@ -27,7 +27,12 @@ const { doubleCsrfProtection, generateToken } = doubleCsrf({
     if (!csrfTokenSecret) throw new Error('CSRF_TOKEN_SECRET not defined')
 
     return csrfTokenSecret
-  }
+  },
+  errorConfig: {
+    statusCode: 403,
+    message: 'CSRF token not provided. Please, request a new CSRF token at /csrf-token.'
+  },
+
 })
 app.use(express.json())
 const myRoute = (req, res) => {
@@ -37,6 +42,17 @@ const myRoute = (req, res) => {
 app.get('/csrf-token', myRoute)
 app.use(doubleCsrfProtection)
 app.use(helmet())
+app.use((error, request, response, next) => {
+  const errorType = String(error).split(':').shift()
+  const errorMessage = String(error).split(' ').slice(1).join(' ')
+
+  const csrfErrorMessage = 'CSRF token not provided. Please, request a new CSRF token at /csrf-token.'
+  const csrfErrorType = 'ForbiddenError'
+
+  if (errorType === csrfErrorType && errorMessage === csrfErrorMessage) {
+    response.status(403).json({ message: errorMessage })
+  }
+})
 
 const expenseRepository = new ExpenseRepository()
 const addExpenseController = new AddExpenseController(expenseRepository)
