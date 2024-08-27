0dc5140aa908c59c46bc72a8a991883b6d4256ba
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-27 09:33:09 -0300

    test: given CSRF middleware, when csrf token is not provided in cookies, then should return a message error

--- src/middleware/csrf.spec.ts
+++ src/middleware/csrf.spec.ts
@@ -33,12 +33,28 @@ describe('CSRF Middleware', () => {
   it('when CSRF token is invalid, then should return a message error', async () => {
     const response = await request(app)
       .post('/v1/expenses')
+      .set('Cookie', '__Host-psifi.x-csrf-token=invalid-csrf-token')
       .set('x-csrf-token', 'invalid-csrf-token')
       .send({})
 
     const responseBody: MessageError = response.body
 
     expect(responseBody.message).toEqual('CSRF token provided is invalid. Please, request a new CSRF token at /csrf-token.')
+  })
+
+  it('when CSRF token is not provided in cookies, then should return a message error', async () => {
+    const csrfToken = await request(app)
+      .get('/csrf-token')
+      .then(response => response.body.csrfToken)
+
+    const response = await request(app)
+      .post('/v1/expenses')
+      .set('x-csrf-token', csrfToken)
+      .send({})
+
+    const requestBody: MessageError = response.body
+
+    expect(requestBody.message).toEqual('CSRF token not provided in cookies. Please, request a new CSRF token at /csrf-token.')
   })
 
   it('when CSRF token is provided and valid, then shouldn\'t return any errors', async () => {
