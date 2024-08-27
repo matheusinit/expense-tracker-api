3be85cacb496e3b97049432299c5e2cb8adc0d52
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-26 22:39:00 -0300

    test: given CSRF middleware, when csrf token is missing, then should return a message error

--- src/middleware/csrf.spec.ts
+++ src/middleware/csrf.spec.ts
@@ -2,10 +2,22 @@ import { describe, it, expect } from 'vitest'
 import request from 'supertest'
 import app from '../app'
 
+type MessageError = {
+  message: string
+}
+
 describe('CSRF Middleware', () => {
   it('when CSRF token is missing, then should return 403 status', async () => {
     const response = await request(app).post('/v1/expenses').send({})
 
     expect(response.status).toEqual(403)
+  })
+
+  it('when CSRF token is missing, then should return a message error', async () => {
+    const response = await request(app).post('/v1/expenses').send({})
+
+    const requestBody: MessageError = response.body
+
+    expect(requestBody.message).toEqual('CSRF token not provided. Please, request a new CSRF token at /csrf-token.')
   })
 })
