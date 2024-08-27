3b500218d472d7854aa247be556c6133b69081c9
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-27 09:42:36 -0300

    refactor: merge tests checking error messages and status code to not make tests to generic on testing only status code

--- src/middleware/csrf.spec.ts
+++ src/middleware/csrf.spec.ts
@@ -7,30 +7,16 @@ type MessageError = {
 }
 
 describe('CSRF Middleware', () => {
-  it('when CSRF token is not provided in HTTP headers, then should return 403 status', async () => {
-    const response = await request(app).post('/v1/expenses').send({})
-
-    expect(response.status).toEqual(403)
-  })
-
-  it('when CSRF token is not provided in HTTP headers, then should return a message error', async () => {
+  it('when CSRF token is not provided in HTTP headers, then should return forbidden with a message error', async () => {
     const response = await request(app).post('/v1/expenses').send({})
 
     const requestBody: MessageError = response.body
 
+    expect(response.status).toEqual(403)
     expect(requestBody.message).toEqual('CSRF token not provided. Please, request a new CSRF token at /csrf-token.')
   })
 
-  it('when CSRF token is invalid, then should return forbidden status code', async () => {
-    const response = await request(app)
-      .post('/v1/expenses')
-      .set('x-csrf-token', 'invalid-csrf-token')
-      .send({})
-
-    expect(response.status).toEqual(403)
-  })
-
-  it('when CSRF token is invalid, then should return a message error', async () => {
+  it('when CSRF token is invalid, then should return forbidden with a message error', async () => {
     const response = await request(app)
       .post('/v1/expenses')
       .set('Cookie', '__Host-psifi.x-csrf-token=invalid-csrf-token')
@@ -39,6 +25,7 @@ describe('CSRF Middleware', () => {
 
     const responseBody: MessageError = response.body
 
+    expect(response.status).toEqual(403)
     expect(responseBody.message).toEqual('CSRF token provided is invalid. Please, request a new CSRF token at /csrf-token.')
   })
 
