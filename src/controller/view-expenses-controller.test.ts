799af87c879d4a3a6e14275424bf2861d94f4889
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-29 23:14:40 -0300

    test: given view expenses controller, when an expense is added, then should return the expense in the offset pagination

--- src/controller/view-expenses-controller.test.ts
+++ src/controller/view-expenses-controller.test.ts
@@ -47,4 +47,30 @@ describe('Given view expenses controller', () => {
     expect(responseBody._metadata.page_count).toBeTypeOf('number')
     expect(responseBody._metadata.total_count).toBeTypeOf('number')
   })
+
+  it('when an expense is added, then should return the expense in the offset pagination', async () => {
+    const expense = {
+      description: falso.randProductName(),
+      amount: falso.randAmount({ fraction: 0 })
+    }
+
+    const csrfResponse = await request(app).get('/csrf-token')
+    const csrfToken = csrfResponse.body['csrfToken']
+
+    const cookies = csrfResponse.headers['set-cookie'].at(0) ?? ''
+
+    await request(app)
+      .post('/v1/expenses')
+      .set('Cookie', cookies)
+      .set('x-csrf-token', csrfToken)
+      .send(expense)
+
+    const response = await request(app)
+      .get('/v1/expenses')
+
+    const responseBody: OffsetPaginationDTO = response.body
+
+    expect(response.status).toBe(200)
+    expect(responseBody.records).toEqual(expect.arrayContaining([expect.objectContaining(expense)]))
+  })
 })
