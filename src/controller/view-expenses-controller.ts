f06de637af671be1f4079d77f8f68bce6a49f237
matheusinit <matheus.oliveira.s@protonmail.com>
2024-08-30 12:36:49 -0300

    feat: given view expenses controller, when multiple expenses is added, then should return expenses in multiple pages

--- src/controller/view-expenses-controller.ts
+++ src/controller/view-expenses-controller.ts
@@ -3,13 +3,21 @@ import db from '../database'
 
 class ViewExpensesController {
   async handle(request: Request, response: Response) {
-    const expenses = await db.expense.findMany()
+    const expenses = await db.expense.findMany({
+      take: 5,
+      skip: 0,
+    })
+    const totalCount = await db.expense.count()
+
+    const page = 1
+    const perPage = 5
+    const pageCount = Math.ceil(totalCount / perPage)
 
     const metadata = {
-      page: 1,
-      per_page: 10,
-      page_count: 2,
-      total_count: expenses.length,
+      page: page,
+      per_page: perPage,
+      page_count: pageCount,
+      total_count: totalCount,
     }
 
     return response.json({ records: expenses, _metadata: metadata })
