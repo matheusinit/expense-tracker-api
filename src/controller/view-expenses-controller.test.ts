93d77d9ea8037625e1cfa8345a3f79cfb96950bd
matheusinit <matheus.oliveira.s@protonmail.com>
2024-09-11 22:42:38 -0300

    test: given view expenses controller, when specify the multiple fields filter, then should return only the fields that match the filter

--- src/controller/view-expenses-controller.test.ts
+++ src/controller/view-expenses-controller.test.ts
@@ -246,4 +246,39 @@ describe('Given view expenses controller', () => {
       }
     ]))
   })
+
+  it('when specify multiple fields filter, then should return only the fields that match the filter', async () => {
+    const expenses = generateExpenses(15)
+
+    const { csrfToken, cookies } = await getCSRFTokenAndCookies()
+
+    for (const expense of expenses) {
+      await request(app)
+        .post('/v1/expenses')
+        .set('x-csrf-token', csrfToken)
+        .set('Cookie', cookies)
+        .send(expense)
+    }
+
+    const query = {
+      fields: 'id, description, amount, createdAt, updatedAt'
+    }
+
+    const response = await request(app)
+      .get('/v1/expenses')
+      .query(query)
+
+    const responseBody: PageBasedPaginationDTO = response.body
+
+    expect(response.status).toBe(200)
+    expect(responseBody.records).toEqual(expect.arrayContaining([
+      {
+        id: expect.any(String),
+        description: expect.any(String),
+        amount: expect.any(Number),
+        createdAt: expect.any(String),
+        updatedAt: expect.any(String)
+      }
+    ]))
+  })
 })
