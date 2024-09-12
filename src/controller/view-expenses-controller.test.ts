51190c73d448355cb5fef140cf7d89e0b3be60a3
matheusinit <matheus.oliveira.s@protonmail.com>
2024-09-11 22:37:30 -0300

    test: given view expenses controller, when specify the fields filter, then should return only the fields that match the filter

--- src/controller/view-expenses-controller.test.ts
+++ src/controller/view-expenses-controller.test.ts
@@ -214,4 +214,36 @@ describe('Given view expenses controller', () => {
     expect(responseBody._metadata.per_page).toEqual(pageSize)
     expect(responseBody._metadata.total_count).toEqual(expenses.length)
   })
+
+  it('when specify the fields filter, then should return only the fields that match the filter', async () => {
+    const expenses = generateExpenses(15)
+
+    const { csrfToken, cookies } = await getCSRFTokenAndCookies()
+
+    for (const expense of expenses) {
+      await request(app)
+        .post('/v1/expenses')
+        .set('x-csrf-token', csrfToken)
+        .set('Cookie', cookies)
+        .send(expense)
+    }
+
+    const query = {
+      fields: 'description, amount'
+    }
+
+    const response = await request(app)
+      .get('/v1/expenses')
+      .query(query)
+
+    const responseBody: PageBasedPaginationDTO = response.body
+
+    expect(response.status).toBe(200)
+    expect(responseBody.records).toEqual(expect.arrayContaining([
+      {
+        description: expect.any(String),
+        amount: expect.any(Number)
+      }
+    ]))
+  })
 })
