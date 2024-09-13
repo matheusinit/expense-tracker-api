df5fec1b735eac372ed1ad0bdbd31c795df9d121
matheusinit <matheus.oliveira.s@protonmail.com>
2024-09-13 16:57:42 -0300

    feat: given cookie middleware, when cookie options is passed, should parse cookies from header to request.cookies

--- src/middleware/cookie.ts
+++ src/middleware/cookie.ts
@@ -18,7 +18,45 @@ export const parseCookies = (request: IncomingMessage, response: ServerResponse,
     return next()
   }
 
-  cookie.split(';').forEach((cookie) => {
+  const cookieSplitted = cookie.split(';')
+
+  if (cookieSplitted.find((cookie) => cookie.trim().startsWith('Path='))) {
+    const index = cookieSplitted.findIndex(cookie => cookie.trim().startsWith('Path='))
+
+    cookieSplitted.splice(index, 1)
+  }
+
+  if (cookieSplitted.find((cookie) => cookie.trim() == 'Secure')) {
+    const index = cookieSplitted.findIndex(cookie => cookie.trim() == 'Secure')
+
+    cookieSplitted.splice(index, 1)
+  }
+
+  if (cookieSplitted.find((cookie) => cookie.trim() == 'HttpOnly')) {
+    const index = cookieSplitted.findIndex(cookie => cookie.trim() == 'HttpOnly')
+
+    cookieSplitted.splice(index, 1)
+  }
+
+  if (cookieSplitted.find((cookie) => cookie.trim().startsWith('SameSite='))) {
+    const index = cookieSplitted.findIndex(cookie => cookie.trim().startsWith('SameSite='))
+
+    cookieSplitted.splice(index, 1)
+  }
+
+  if (cookieSplitted.find((cookie) => cookie.trim().startsWith('Domain='))) {
+    const index = cookieSplitted.findIndex(cookie => cookie.trim().startsWith('Domain='))
+
+    cookieSplitted.splice(index, 1)
+  }
+
+  if (cookieSplitted.find((cookie) => cookie.trim().startsWith('Max-Age='))) {
+    const index = cookieSplitted.findIndex(cookie => cookie.trim().startsWith('Max-Age='))
+
+    cookieSplitted.splice(index, 1)
+  }
+
+  cookieSplitted.forEach((cookie) => {
     const [key, value] = cookie.split('=')
     request.cookies = defaultCookies
     request.cookies[key.trim()] = String(value.trim())
