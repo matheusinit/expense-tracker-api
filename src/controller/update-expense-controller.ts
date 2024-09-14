4c965a814fec2a2a708b52f1a09a2ba0486ca87a
matheusinit <matheus.oliveira.s@protonmail.com>
2024-09-13 22:34:17 -0300

    feat: given update expense controller, when is given a single field to update, then should return the resource with updated field

--- src/controller/update-expense-controller.ts
+++ src/controller/update-expense-controller.ts
@@ -13,7 +13,20 @@ class UpdateExpenseController {
       return response.status(404).send({ message: 'Expense not found' })
     }
 
-    return response.status(400).send({ message: 'At least one field must be provided' })
+    if (request.body['description'] === undefined && request.body['amount'] === undefined) {
+      return response.status(400).send({ message: 'At least one field must be provided' })
+    }
+
+    const expenseUpdated = await db.expense.update({
+      where: {
+        id: expenseFound.id
+      },
+      data: {
+        amount: Number(request.body.amount)
+      }
+    })
+
+    return response.status(200).send(expenseUpdated)
   }
 }
 
