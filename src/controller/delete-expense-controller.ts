610e3f4764e56b41b9511499a6cf14196ddeccfd
matheusinit <matheus.oliveira.s@protonmail.com>
2024-09-16 18:24:37 -0300

    feat: given remove expense controller, when is given an id of resource already deleted, then should return not found error

--- src/controller/delete-expense-controller.ts
+++ src/controller/delete-expense-controller.ts
@@ -1,9 +1,24 @@
+import db from '@/database'
 import { Request, Response } from 'express'
 
 class DeleteExpenseController {
   async handle(request: Request, response: Response) {
+    const { id } = request.params
+
+    const expense = await db.expense.findUnique({
+      where: {
+        id
+      }
+    })
+
+    if (!expense) {
+      return response.status(404).send({
+        message: 'Expense not found'
+      })
+    }
+
     return response.status(404).send({
-      message: 'Expense not found'
+      message: 'Resource already deleted'
     })
   }
 }
