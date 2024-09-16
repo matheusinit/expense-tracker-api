d9dfdab2c8e3e9117b0eedb2e16b0fc25d229f7e
matheusinit <matheus.oliveira.s@protonmail.com>
2024-09-16 18:49:04 -0300

    refactor: move all routes and dependencies into a module responsible with routes

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,9 @@ import { applyCsrfTokenController } from '@/controller/csrf-token-controller'
 import { csrf } from '@/middleware/csrf'
 import { serverSession } from '@/middleware/session'
 import { logger } from '@/middleware/logger'
-import { makeAddExpenseController } from '@/factory/add-expense-controller-factory'
-import ViewExpensesController from '@/controller/view-expenses-controller'
-import UpdateExpenseController from '@/controller/update-expense-controller'
 import { parseCookies } from '@/middleware/cookie'
-import ExpenseRepository from './repository/expense-repository'
-import DeleteExpenseController from './controller/delete-expense-controller'
+
+import expenseRouter from '@/routes'
 
 const app = express()
 
@@ -26,18 +23,6 @@ app.use(csrf)
 app.get('/csrf-token', applyCsrfTokenController)
 app.use(applyCustomCsrfErrors)
 
-const viewExpensesController = new ViewExpensesController()
-const expenseRepository = new ExpenseRepository()
-const updateExpenseController = new UpdateExpenseController(expenseRepository)
-const deleteExpenseController = new DeleteExpenseController(expenseRepository)
-
-const router = express.Router()
-
-router.get('/expenses', (request, response) => viewExpensesController.handle(request, response))
-router.post('/expenses', (request, response) => makeAddExpenseController().handle(request, response))
-router.put('/expenses/:id', (request, response) => updateExpenseController.handle(request, response))
-router.delete('/expenses/:id', (request, response) => deleteExpenseController.handle(request, response))
-
-app.use('/v1', router)
+app.use('/v1', expenseRouter)
 
 export default app
