dfb9e15193013abd81f205f0c503ea81f61c0e34
matheusinit <matheus.oliveira.s@protonmail.com>
2024-09-16 18:24:37 -0300

    test: given remove expense controller, when is given a non-valid id, then should return not found error

---
+++ src/controller/delete-expense-controller.test.ts
@@ -0,0 +1,51 @@
+import { afterAll, afterEach, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import * as falso from '@ngneat/falso'
+
+import app from '@/app'
+import db from '@/database'
+import { generateExpenses } from '@/utils/tests/generate-expenses'
+import { getCSRFTokenAndCookies } from '@/utils/tests/get-csrf-token-and-cookies'
+import { MessageErrorDTO } from '@/dtos/error-message'
+
+describe('Given remove expense controller', () => {
+  beforeAll(async () => {
+    await db.$connect()
+  })
+
+  afterEach(async () => {
+    await db.expense.deleteMany({})
+  })
+
+  afterAll(async () => {
+    await db.$disconnect()
+  })
+
+  it('when is given a non-valid id, then should return not found error', async () => {
+    const expenses = generateExpenses(10)
+
+    const { csrfToken, cookies } = await getCSRFTokenAndCookies()
+
+    for (const expense of expenses) {
+      await request(app)
+        .post('/v1/expenses')
+        .set('x-csrf-token', csrfToken)
+        .set('Cookie', cookies)
+        .send(expense)
+    }
+
+    const id = falso.randUuid()
+
+    const response = await request(app)
+      .delete(`/v1/expenses/${id}`)
+      .set('x-csrf-token', csrfToken)
+      .set('Cookie', cookies)
+      .send()
+
+    const responseBody: MessageErrorDTO = response.body
+
+    expect(response.status).toBe(404)
+    expect(responseBody.message).toBe('Expense not found')
+  })
+
+})
