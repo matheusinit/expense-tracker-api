4f051b0e987e19f28dfc649bb0a555e1f1cffb8d
matheusinit <matheus.oliveira.s@protonmail.com>
2024-09-17 08:46:52 -0300

    feat: given remove expense controller, when a error is thrown, then should return internal server error

--- src/controller/delete-expense-controller.ts
+++ src/controller/delete-expense-controller.ts
@@ -9,25 +9,31 @@ class DeleteExpenseController {
   }
 
   async handle(request: Request, response: Response) {
-    const { id } = request.params
+    try {
+      const { id } = request.params
 
-    const expense = await this.repository.get(id)
+      const expense = await this.repository.get(id)
 
-    if (!expense) {
-      return response.status(404).send({
-        message: 'Expense not found'
+      if (!expense) {
+        return response.status(404).send({
+          message: 'Expense not found'
+        })
+      }
+
+      await this.repository.delete(id)
+
+      if (expense.deletedAt) {
+        return response.status(404).send({
+          message: 'Resource already deleted'
+        })
+      }
+
+      return response.status(204).send()
+    } catch (_err) {
+      return response.status(500).send({
+        message: 'Internal server error'
       })
     }
-
-    await this.repository.delete(id)
-
-    if (expense.deletedAt) {
-      return response.status(404).send({
-        message: 'Resource already deleted'
-      })
-    }
-
-    return response.status(204).send()
   }
 }
 
