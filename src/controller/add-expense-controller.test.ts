d7fb091f965bbe5db8495cc5726f536c556793d4
matheusinit <matheus.oliveira.s@protonmail.com>
2024-09-17 18:03:00 -0300

    test: given add expense controller, when due date is a negative, then should return bad request error

--- src/controller/add-expense-controller.test.ts
+++ src/controller/add-expense-controller.test.ts
@@ -209,4 +209,25 @@ describe('Given add expense controller', () => {
     expect(response.status).toEqual(400)
     expect(responseBody.message).toEqual('Invalid value for dueDate. It should be in interval of days of a month.')
   })
+
+  it('when due date is a negative, then should return bad request error', async () => {
+    const expense = {
+      amount: '100',
+      description: falso.randProductName(),
+      dueDate: falso.randNumber({ min: -9999, max: -1 })
+    }
+
+    const { cookies, csrfToken } = await getCSRFTokenAndCookies()
+
+    const response = await request(app)
+      .post('/v1/expenses')
+      .set('Cookie', cookies)
+      .set('x-csrf-token', csrfToken)
+      .send(expense)
+
+    const responseBody: MessageErrorDTO = response.body
+
+    expect(response.status).toEqual(400)
+    expect(responseBody.message).toEqual('Invalid value for dueDate. It should be in interval of days of a month.')
+  })
 })
