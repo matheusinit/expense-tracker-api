4fac469133639f8f8e68d1d7eae039ebdcbf5a2b
matheusinit <matheus.oliveira.s@protonmail.com>
2024-09-18 23:55:31 -0300

    refactor: remove factory keyword in function, use make verb as synonym to factory

--- src/factory/update-expense-controller-factory.ts
+++ src/factory/update-expense-controller-factory.ts
@@ -1,7 +1,7 @@
 import UpdateExpenseController from '@/controller/update-expense-controller'
 import ExpenseRepository from '@/repository/expense-repository'
 
-export const makeUpdateExpenseControllerFactory = () => {
+export const makeUpdateExpenseController = () => {
   const expenseRepository = new ExpenseRepository()
   const updateExpenseController = new UpdateExpenseController(expenseRepository)
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,14 +2,14 @@ import { Router } from 'express'
 
 import { makeAddExpenseController } from '@/factory/add-expense-controller-factory'
 import { makeDeleteExpenseController } from '@/factory/delete-expense-controller-factory'
-import { makeUpdateExpenseControllerFactory } from '@/factory/update-expense-controller-factory'
+import { makeUpdateExpenseController } from '@/factory/update-expense-controller-factory'
 import { makeViewExpensesController } from '@/factory/view-expense-controller-factory'
 
 const router = Router()
 
 router.get('/expenses', (request, response) => makeViewExpensesController().handle(request, response))
 router.post('/expenses', (request, response) => makeAddExpenseController().handle(request, response))
-router.put('/expenses/:id', (request, response) => makeUpdateExpenseControllerFactory().handle(request, response))
+router.put('/expenses/:id', (request, response) => makeUpdateExpenseController().handle(request, response))
 router.delete('/expenses/:id', (request, response) => makeDeleteExpenseController().handle(request, response))
 
 export default router
