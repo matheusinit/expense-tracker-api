d99108dd718a03940a778e2f6dfe443517933128
suetham <matheus.oliveira.s@protonmail.com>
2024-09-22 11:34:28 -0300

    test: given is needed to schedule expenses, when passed valid expenses, should be able to schedule

--- src/data/entities/expense-schedule.spec.ts
+++ src/data/entities/expense-schedule.spec.ts
@@ -12,4 +12,15 @@ describe('Given is needed to schedule expenses', () => {
     expect(expenseSchedule.expenses).toEqual([expense])
 
   })
+
+  it('when passed valid expenses, should be able to schedule', () => {
+    const expense1 = new Expense('Credit card bill', 100, 10)
+    const expense2 = new Expense('Internet bill', 50, 10)
+    const expenseSchedule = new ExpenseSchedule()
+
+    expenseSchedule.include(expense1)
+    expenseSchedule.include(expense2)
+
+    expect(expenseSchedule.expenses).toEqual([expense1, expense2])
+  })
 })
