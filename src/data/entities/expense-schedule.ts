19b89972bc164326b211d784f68a6ce942dc5612
suetham <matheus.oliveira.s@protonmail.com>
2024-09-22 12:11:50 -0300

    feat: given is needed to schedule expenses, when expenses are included with due date below than current date, should schedule to next month

--- src/data/entities/expense-schedule.ts
+++ src/data/entities/expense-schedule.ts
@@ -3,6 +3,7 @@ import { Expense } from './expense'
 
 export class ExpenseSchedule {
   private readonly _expenses: Expense[]
+  private _month: string | undefined
 
   constructor() {
     this._expenses = []
@@ -10,10 +11,24 @@ export class ExpenseSchedule {
 
   include(expense: Expense) {
     this._expenses.push(expense)
+
+    const date = new Date()
+    const currentMonth = date.getMonth()
+    const currentDate = date.getDate()
+
+    this.expenses.map(expense => {
+      if (expense.dueDate <= currentDate) {
+        this._month = new Date(date.getFullYear(), currentMonth + 1, 1).toLocaleString('default', { month: 'long' })
+      }
+    })
   }
 
   get expenses(): Expense[] {
     return this._expenses
+  }
+
+  get month() {
+    return this._month
   }
 }
 
