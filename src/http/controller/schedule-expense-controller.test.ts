5b3daeb1a599508ce8dcfa77cef93814a931eab5
matheusinit <matheus.oliveira.s@protonmail.com>
2024-10-01 08:45:33 -0300

    test: given schedule expenses controller, when is provided a expense, then the id should not be equal to expense id

--- src/http/controller/schedule-expense-controller.test.ts
+++ src/http/controller/schedule-expense-controller.test.ts
@@ -32,12 +32,39 @@ describe('Given schedule expenses controller', () => {
     expect(response.statusCode).toEqual(201)
     expect(responseBody).toEqual(expect.objectContaining({
       id: expect.any(String),
-      description: expense.description,
-      totalAmount: expense.amount,
-      status: expense.dueDate,
+      description: expect.any(String),
+      totalAmount: expense.amount * 100,
+      status: expect.any(String),
       createdAt: expect.any(String),
       updatedAt: expect.any(String),
       deletedAt: null
     }))
+  })
+
+  it('when is provided a expense, then the id should not be equal to expense id', async () => {
+    const expense = {
+      description: 'Credit card bill',
+      amount: 100,
+      dueDate: 10
+    }
+
+    const { cookies, csrfToken } = await getCSRFTokenAndCookies()
+
+    const expenseResponse = await request(app)
+      .post('/v1/expenses')
+      .set('Cookie', cookies)
+      .set('x-csrf-token', csrfToken)
+      .send(expense)
+
+    const expenseId = expenseResponse.body['id']
+
+    const response = await request(app)
+      .post(`/v1/expenses/${expenseId}/schedule`)
+      .set('Cookie', cookies)
+      .set('x-csrf-token', csrfToken)
+
+    const responseBody = response.body
+
+    expect(responseBody.id).not.toEqual(expenseId)
   })
 })
