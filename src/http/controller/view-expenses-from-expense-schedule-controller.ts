12efca96aa3073c9d49ef57295f2f2ec79e5a0ad
matheusinit <matheus.oliveira.s@protonmail.com>
2024-10-15 09:01:09 -0300

    feat: given a valid id, when attempts to retrieve the resource associated with id, then should return ok status

--- src/http/controller/view-expenses-from-expense-schedule-controller.ts
+++ src/http/controller/view-expenses-from-expense-schedule-controller.ts
@@ -1,10 +1,25 @@
+import { ExpenseScheduleRepository } from '@/data/protocols/expense-schedule-repository'
 import { Request, Response } from 'express'
 
 class ViewExpensesFromExpenseScheduleController {
+  private readonly repository: ExpenseScheduleRepository
+
+  constructor(repository: ExpenseScheduleRepository) {
+    this.repository = repository
+  }
+
   async handle(request: Request, response: Response) {
-    return response.status(404).send({
-      message: 'Expense schedule not found'
-    })
+    const id = request.params['id']
+
+    const expenseSchedule = await this.repository.getById(id)
+
+    if (!expenseSchedule) {
+      return response.status(404).send({
+        message: 'Expense schedule not found'
+      })
+    }
+
+    return response.status(200).send()
   }
 }
 
--- src/http/routes/schedules.ts
+++ src/http/routes/schedules.ts
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import { makeDetailExpenseScheduleController } from '@/http/factory/detail-expense-schedule-controller-factory'
 import ViewExpensesFromExpenseScheduleController from '@/http/controller/view-expenses-from-expense-schedule-controller'
+import { ExpenseScheduleRepositoryRelationalDatabase } from '@/infra/database/repository/expense-schedule-repository'
 
 const router = Router()
 
 router.get('/:id', (request, response) => makeDetailExpenseScheduleController().handle(request, response))
-router.get('/:id/expenses', (request, response) => (new ViewExpensesFromExpenseScheduleController).handle(request, response))
+const repository = new ExpenseScheduleRepositoryRelationalDatabase()
+const controller = new ViewExpensesFromExpenseScheduleController(repository)
+router.get('/:id/expenses', (request, response) => controller.handle(request, response))
 
 export { router }
