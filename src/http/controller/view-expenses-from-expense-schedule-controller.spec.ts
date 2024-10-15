1e089cb7593f1ab9f5070082c9963f0b0c0401d6
matheusinit <matheus.oliveira.s@protonmail.com>
2024-10-15 09:22:36 -0300

    test: given a valid id, when attempts to retrieve the resource associated with id, then should return pagination metadata

--- src/http/controller/view-expenses-from-expense-schedule-controller.spec.ts
+++ src/http/controller/view-expenses-from-expense-schedule-controller.spec.ts
@@ -152,4 +152,64 @@ describe('View expenses from expense schedule controller', () => {
       expect.objectContaining({ id: expense2Id })
     ]))
   })
+
+  it('given a valid id, when attempts to retrieve the resource associated with id, then should return pagination metadata', async () => {
+    vi.setSystemTime(new Date('2024-10-04'))
+    const expense = {
+      description: 'Credit card',
+      amount: 95.50,
+      dueDate: 10
+    }
+
+    const { cookies, csrfToken } = await getCSRFTokenAndCookies()
+
+    const expenseResponse1 = await request(app)
+      .post('/v1/expenses')
+      .set('Cookie', cookies)
+      .set('x-csrf-token', csrfToken)
+      .send(expense)
+
+    const expenseId = expenseResponse1.body.id
+
+    await request(app)
+      .post(`/v1/expenses/${expenseId}/schedules`)
+      .set('Cookie', cookies)
+      .set('x-csrf-token', csrfToken)
+      .send()
+
+    const expense2 = {
+      description: 'Gym',
+      amount: 85.45,
+      dueDate: 10
+    }
+
+    const expenseResponse2 = await request(app)
+      .post('/v1/expenses')
+      .set('Cookie', cookies)
+      .set('x-csrf-token', csrfToken)
+      .send(expense2)
+
+    const expense2Id = expenseResponse2.body.id
+
+    const expenseScheduleResponse = await request(app)
+      .post(`/v1/expenses/${expense2Id}/schedules`)
+      .set('Cookie', cookies)
+      .set('x-csrf-token', csrfToken)
+      .send()
+
+    const id = expenseScheduleResponse.body.id
+
+    const response = await request(app)
+      .get(`/v1/schedules/${id}/expenses`)
+      .send()
+
+    const responseBody: PageBasedPagination<ExpenseModel> = response.body
+
+    expect(responseBody._metadata).toEqual({
+      page: 1,
+      per_page: 5,
+      page_count: 1,
+      total_count: 2
+    })
+  })
 })
