ed85aa87407d3c6e57facc3ac7ea09c387a5507b
matheusinit <matheus.oliveira.s@protonmail.com>
2024-10-17 22:43:15 -0300

    refactor: create a factory function to create controller to view expenses from expense schedule

--- src/http/routes/schedules.ts
+++ src/http/routes/schedules.ts
@@ -1,15 +1,10 @@
 import { Router } from 'express'
 import { makeDetailExpenseScheduleController } from '@/http/factory/detail-expense-schedule-controller-factory'
-import ViewExpensesFromExpenseScheduleController from '@/http/controller/view-expenses-from-expense-schedule-controller'
-import { ExpenseScheduleRepositoryRelationalDatabase } from '@/infra/database/repository/expense-schedule-repository'
-import ExpenseRepositoryRelationalDatabase from '@/infra/database/repository/expense-repository'
+import { makeViewExpensesFromExpenseScheduleController } from '@/http/factory/view-expenses-from-expense-schedule-controller-factory'
 
 const router = Router()
 
 router.get('/:id', (request, response) => makeDetailExpenseScheduleController().handle(request, response))
-const repository = new ExpenseScheduleRepositoryRelationalDatabase()
-const expenseRepository = new ExpenseRepositoryRelationalDatabase()
-const controller = new ViewExpensesFromExpenseScheduleController(repository, expenseRepository)
-router.get('/:id/expenses', (request, response) => controller.handle(request, response))
+router.get('/:id/expenses', (request, response) => makeViewExpensesFromExpenseScheduleController().handle(request, response))
 
 export { router }
